86bff129bdbdcca3c1459754455d8eb0190b92d6
Tim <acronym1986@gmail.com>
2021-03-18 20:14:34 +1100

    code added and passed test

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+//1
+const currentUser = "Tim";
+ 
+//2
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+ 
+//3
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//4 
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`; 
